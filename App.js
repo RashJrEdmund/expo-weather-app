51042713f727ebaeab52cfd0aa4d300a5b2f3938
rash <orashusedmund@gmail.com>
2023-07-15 12:52:37 +0100

    done with basic setup and building ui

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text style={{color: "#fff"}}>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+       <Text style={styles.temp}>6</Text>
+       <Text style={styles.feels}>Feels like 5</Text>
+
+       <View style={styles.highLowWrapper}>
+          <Text style={styles.high}>High: 8</Text>
+          <Text style={styles.low}>Low: 6</Text>
+       </View>
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: 'steelblue',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  temp: {
+    fontSize: 40,
+    marginTop: 15,
+    color: "#fff",
+  },
+  feels:{
+    fontSize: 30,
+    borderBottomColor: "#000"
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+    color: "#000",
+    fontSize: 20,
+  },
+  high: {
+    fontSize: 40,
+    color: "#fff",
+  },
+  low:{
+    fontSize: 20,
+  }
 });
